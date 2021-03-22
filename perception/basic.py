8f7068a66ef2c457aa4db8d4fa96dc53e88ce0dc
xhzhang76 <xhzhang76@outlook.com>
2021-03-22 22:38:40 +0800

    add network

---
+++ perception/basic.py
@@ -0,0 +1,125 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# def AND(x1, x2):
+#     w1, w2, theta = 0.5, 0.5, 0.7
+#     tmp = w1 * x1 + w2 * x2
+#     if tmp <= theta:
+#         return 0
+#     else:
+#         return 1
+
+# AND/NAND/OR单层感知机，XOR二层感知机
+# 组合NAND，感知机实现计算机
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    weight = np.array([0.5, 0.5])
+    bias = -0.7
+    tmp = np.sum(weight * x) + bias
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    weight = np.array([-0.5, -0.5])
+    bias = 0.7
+    tmp = np.sum(weight * x) + bias
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    weight = np.array([0.5, 0.5])
+    bias = -0.2
+    tmp = np.sum(weight * x) + bias
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+# 输入层-中间层（隐藏层）-输出层
+# y=h(w1*x1+w2*x2+b)   h激活函数（阶跃-感知机，非阶跃-神经网络）
+# sigmoid：h(x)=1/(1+exp(-x))
+# ReLU(Rectified Linear Unit)
+
+
+# def step_function(x):
+#     """
+#     Only support float parameter
+#     """
+#     if x > 0:
+#         return 1
+#     else:
+#         return 0
+
+
+# def step_function(x):
+#     y = x > 0
+#     return y.astype(np.int)
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=int)
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def identity_function(x):
+    return x
+
+
+if __name__ == '__main__':
+    print(XOR(0, 0))
+    print(XOR(0, 1))
+    print(XOR(1, 0))
+    print(XOR(1, 1))
+
+    x = np.arange(-30.0, 30.0, 0.1)
+    y1 = step_function(x)
+    y2 = sigmoid(x)
+    y3 = relu(x)
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.plot(x, y3)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+
+    X = np.array([1.0, 0.5])
+    W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    B1 = np.array([0.1, 0.2, 0.3])
+    A1 = np.dot(X, W1) + B1  # [0.3 0.7 1.1]
+    Z1 = sigmoid(A1)  # [0.57444252 0.66818777 0.75026011]
+
+    W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    B2 = np.array([0.1, 0.2])
+    A2 = np.dot(Z1, W2) + B2  # [0.51615984 1.21402696]
+    Z2 = sigmoid(A2)  # [0.62624937 0.7710107 ]
+
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    B3 = np.array([0.1, 0.2])
+    A3 = np.dot(Z2, W3) + B3  # [0.31682708 0.69627909]
+    Y = identity_function(A3)  # [0.31682708 0.69627909]
+
--- test_numpy.py
+++ test_numpy.py
@@ -1,8 +1,33 @@
-
 import numpy as np
 
-#
-a = np.array([[1,2], [3,4]])
+# 一维——向量，二维——矩阵，一般化统称：张量（tensor）
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
 print(a)
 print(a.shape)  # (2,2)
 print(a.dtype)  # int64
+print(a.ndim)  # 2
+print(np.dot(a, b))
+# [[19 22]
+#  [43 50]]
+
+# 广播
+b = np.array([10, 20])
+print(a * b)
+# [[10 40]
+#  [30 80]]
+
+# slice
+x = np.array([[0, 1], [2, 3], [4, 5]])
+print(x[0])  # [0,1]
+print(x[1, 1])  # 3
+for row in x:
+    print(row)
+print(x.flatten())  # [0 1 2 3 4 5]
+
+# filter index
+print(x >= 3)
+# [[False False]
+#  [False  True]
+#  [ True  True]]
+print(x[x >= 3])  # [3 4 5]
