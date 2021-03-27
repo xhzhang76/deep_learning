5075194dad39d780ce960322dbe40c2e910a313f
xhzhang76 <xhzhang76@outlook.com>
2021-03-27 11:29:12 +0800

    add samples

--- samples/ch03/mnist_show.py
+++ samples/ch03/mnist_show.py
@@ -1,14 +1,17 @@
 # coding: utf-8
-import sys, os
+import os
+import sys
+
 sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
 import numpy as np
-from dataset.mnist import load_mnist
+from samples.dataset.mnist import load_mnist
 from PIL import Image
 
 
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
+
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
